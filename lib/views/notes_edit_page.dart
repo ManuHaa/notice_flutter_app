8a9f1519a878d038c8cf20587b04e4347ff56038
EhmannFlo <79143621+EhmannFlo@users.noreply.github.com>
2021-08-13 20:03:12 +0200

    Add files via upload

---
+++ lib/views/notes_edit_page.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+import 'package:notice_flutter_app/db/notes_database.dart';
+import 'package:notice_flutter_app/model/note.dart';
+import 'package:notice_flutter_app/widgets/note_form_widget.dart';
+
+//view if you touch the add Button
+
+class AddEditNotePage extends StatefulWidget {
+  final Note? note;
+
+  const AddEditNotePage({
+    Key? key,
+    this.note,
+  }) : super(key: key);
+
+  @override
+  _AddEditNotePageState createState() => _AddEditNotePageState();
+}
+
+class _AddEditNotePageState extends State<AddEditNotePage> {
+  final _formKey = GlobalKey<FormState>();
+  late bool isImportant;
+  late int number;
+  late String title;
+  late String description;
+
+  @override
+  void initState() {
+    super.initState();
+
+    isImportant = widget.note?.isImportant ?? false;
+    number = widget.note?.number ?? 0;
+    title = widget.note?.title ?? '';
+    description = widget.note?.description ?? '';
+  }
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+            title: new Text('Notes', style: TextStyle(color: Colors.white)),
+            centerTitle: true,
+            iconTheme: IconThemeData(color: Colors.white)),
+        body: Form(
+          key: _formKey,
+          child: NoteFormWidget(
+            isImportant: isImportant,
+            number: number,
+            title: title,
+            description: description,
+            onChangedImportant: (isImportant) =>
+                setState(() => this.isImportant = isImportant),
+            onChangedNumber: (number) => setState(() => this.number = number),
+            onChangedTitle: (title) => setState(() => this.title = title),
+            onChangedDescription: (description) =>
+                setState(() => this.description = description),
+          ),
+        ),
+        persistentFooterButtons: [buildButton()],
+      );
+
+  Widget buildButton() {
+    return FloatingActionButton.extended(
+      onPressed: () {
+        addOrUpdateNote();
+      },
+      icon: Icon(Icons.save),
+      label: Text('Save'),
+      backgroundColor: Colors.green,
+    );
+  }
+
+  void addOrUpdateNote() async {
+    final isValid = _formKey.currentState!.validate();
+
+    if (isValid) {
+      final isUpdating = widget.note != null;
+
+      if (isUpdating) {
+        await updateNote();
+      } else {
+        await addNote();
+      }
+
+      Navigator.of(context).pop();
+    }
+  }
+
+  Future updateNote() async {
+    final note = widget.note!.copy(
+      isImportant: isImportant,
+      number: number,
+      title: title,
+      description: description,
+    );
+
+    await NotesDatabase.instance.update(note);
+  }
+
+  Future addNote() async {
+    final note = Note(
+      title: title,
+      isImportant: true,
+      number: number,
+      description: description,
+      createdTime: DateTime.now(),
+    );
+
+    await NotesDatabase.instance.create(note);
+  }
+}
